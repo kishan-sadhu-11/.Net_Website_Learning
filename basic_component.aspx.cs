d99300b4fe36935aa4bb26d18c05bad9a5a5beac
KISHAN SADHU <kishansadhusadhu69@gmail.com>
2026-01-09 18:27:45 +0530

    first_commit

---
+++ basic_component.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace basic_component_of_website
+{
+    public partial class basic_component : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
